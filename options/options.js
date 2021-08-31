14e1e4d4e20a2695481917bed7efa381096f1c3d
Crowdbotics <team@crowdbotics.com>
2021-08-31 23:38:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hey_neighbor_30236Identifier",
+  url: "https://hey_neighbor_30236Identifier.botics.co",
+  api: "https://hey_neighbor_30236Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
